9661593df1f38139285ad7d0185e0a6acfc7c69d
Rajitha Peiris <rajithapeiris.personalportfolio@gmail.com>
2023-06-28 14:55:27 +0530

    Add project files.

---
+++ FahrenheitToCelciusConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *  C# Program to Convert Fahrenheit to Celsius 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Celsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            Console.Write("Enter Fahrenheit temperature : ");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("The Converted Celsius Temperature Is: " + celsius);
+            Console.ReadLine();
+        }
+    }
+}
